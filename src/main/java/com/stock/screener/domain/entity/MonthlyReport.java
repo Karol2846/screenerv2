74f007ce4c1df80c6333790fd72e15fc97c5aa1e
Karol2846 <karol200242@gmail.com>
2026-02-25 19:41:49 +0100

    some notes for future

--- src/main/java/com/stock/screener/domain/entity/MonthlyReport.java
+++ src/main/java/com/stock/screener/domain/entity/MonthlyReport.java
@@ -157,3 +157,10 @@ public class MonthlyReport extends PanacheEntity {
                 && upsidePotential != null;
     }
 }
+
+//TODO: new problem to think of:
+// think about how to merge alpha vantage & yhFinance api calls in monthlyReport - to now waste requests from api's
+// MonthlyReportsnapshot requires some data from YhFinance & some from alphaVAntage.
+// It's data will be fetched once a month.
+// but alphaVantage api's data also needs to be fetched once every three months - for quaterly report.
+// Maybe the solution will be to gave data from persist log if current data was already fettched
